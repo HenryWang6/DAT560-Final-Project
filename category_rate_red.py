675c2288b462ffaada0a738245a45fc422d48126
Henry <wanghenry520@gmail.com>
2020-04-29 14:56:33 +0800

    category modify

--- category_rate_red.py
+++ category_rate_red.py
@@ -4,6 +4,7 @@ import sys
 category_view = {}
 category_cart = {}
 category_purchase = {}
+category_ls = []
 
 for line in sys.stdin:
 	category, event_type= line.strip().split(',')
@@ -11,22 +12,25 @@ for line in sys.stdin:
 	if event_type == 'view':
 		try:
 			category_view[category] +=1
-		except ValueError:
+		except KeyError:
 			category_view[category] = 1
+		category_ls.append(category)
 		
 	if event_type =='cart':
 		try:
 			category_cart[category] += 1
 		except ValueError:
 			category_cart[category] = 1
+		category_ls.append(category)
 
 	if event_type =='purchase':
 		try:
 			category_purchase[category] += 1
 		except ValueError:
 			category_purchase = 1
+		category_ls.append(category)
 
 
 print 'category,view_to_cart_rate,cart_to_purchase_rate,purchase_rate'
-for key in category_view_cart.keys():
-	print '%s,%s,%s,%s' % (key, category_cart[key]*100.0/category_view[key], category_purchase[key]*100.0/category_cart[key], category_purchase[key]*100.0/category_view[key])
+for each in category_ls:
+	print '%s,%s,%s,%s' % (each, category_cart[each]*100.0/category_view[each], category_purchase[each]*100.0/category_cart[each], category_purchase[each]*100.0/category_view[each])
