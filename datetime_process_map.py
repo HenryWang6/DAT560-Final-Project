5b55951d267b491409bd248fb34a10363af7afa9
Henry <wanghenry520@gmail.com>
2020-04-29 11:39:53 +0800

    datetime process

--- datetime_process_map.py
+++ datetime_process_map.py
@@ -4,7 +4,10 @@ import sys
 for line in sys.stdin:
 	line_ls = []
 	line_ls.extend(line.strip().split(','))
-
-	date, time, s = line_ls[0].split()
+	try:
+		date, time, s = line_ls[0].split(' ')
+	except ValueError:
+		continue
+	
 
 	print '%s,%s,%s,%s,%s,%s,%s,%s,%s,%s' % (date, time, line_ls[1], line_ls[2], line_ls[3], line_ls[4], line_ls[5], line_ls[6], line_ls[7], line_ls[8])
